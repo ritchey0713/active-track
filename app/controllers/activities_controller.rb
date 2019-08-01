f14774871454f83d3064c17e106cb375aae8915e
Dj Ritchey <ritchey0713@gmail.com>
2019-08-01 10:57:40 -0400

    add activity views

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,38 @@
 class ActivitiesController < ApplicationController
+
+    def index 
+        @activities = Activity.all 
+    end 
+
+    def new 
+        @activity = activity.new
+    end 
+
+    def create 
+
+    end 
+
+    def show 
+
+    end
+    
+    def edit 
+
+    end 
+
+    def update 
+
+    end 
+
+    def destroy 
+
+    end 
+
+    private 
+
+    def set_activity 
+        @activity = Activity.find_by(id: params[:id])
+    end 
+
+
 end
