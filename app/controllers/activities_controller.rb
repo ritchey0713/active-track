38d239c769a0a76e0b8baa0632f987c9231a5bf8
Dj Ritchey <ritchey0713@gmail.com>
2019-08-01 17:14:27 -0400

    add controller blocks

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,4 +1,5 @@
 class ActivitiesController < ApplicationController
+    before_action :set_activity, only: [:show, :edit, :update, :destroy]
 
     def index 
         @activities = Activity.all 
@@ -17,22 +18,26 @@ class ActivitiesController < ApplicationController
         else 
             redirect_to new_activity_path
         end 
-        
+
     end 
 
     def show 
-
     end
     
     def edit 
-
     end 
 
     def update 
-
+        if @activity.update(activity_params)
+            redirect_to activity_path(@activity)
+        else 
+            redirect_to edit_activity_path(@activity)
+        end 
     end 
 
     def destroy 
+        @activity.destroy
+        redirect_to activities_path
 
     end 
 
